ccc4c218cc54cde96fb69bc4fb401eedfb50995c
alloka <alihalawaty@gmail.com>
2020-07-10 19:13:26 +0200

    added content assist

--- rls4eclipse.ide/src-gen/rls4eclipse/ide/contentassist/antlr/internal/InternalRLSLexer.java
+++ rls4eclipse.ide/src-gen/rls4eclipse/ide/contentassist/antlr/internal/InternalRLSLexer.java
@@ -1,5 +1,25 @@
 package rls4eclipse.ide.contentassist.antlr.internal;
 
+/*-
+ * #%L
+ * rls4eclipse.ide
+ * %%
+ * Copyright (C) 2018 - 2020 rls4eclipse Developers
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 // Hack: Use our own Lexer superclass by means of import. 
 // Currently there is no other way to specify the superclass for the lexer.
 import org.eclipse.xtext.ide.editor.contentassist.antlr.internal.Lexer;
